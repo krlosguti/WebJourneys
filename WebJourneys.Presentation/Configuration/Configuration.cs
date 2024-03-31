2f98bc61a5558cb11e8daf0615c71b0c6d6eb647
krlosguti <krodas17@hotmail.com>
2024-03-31 18:15:25 -0500

    #10008 ADD CORS

--- WebJourneys.Presentation/Configuration/Configuration.cs
+++ WebJourneys.Presentation/Configuration/Configuration.cs
@@ -18,6 +18,10 @@ namespace WebJourneys.Presentation.Configuration
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+            {
+                builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+            }));
         }
 
         public static void RegisterMiddlewares(this WebApplication app)
